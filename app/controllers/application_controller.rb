05c2335f91a36bdd3795fd7853adf87952792f96
Librity <luis.geniole@gmail.com>
2019-08-25 14:49:57 -0300

    Add About page, testing & basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,20 @@ class ApplicationController < ActionController::Base
   def test0
     render html: "If you're reading this, application#test0 was created & routed correctly"
   end
+=begin
+  $ rails generate controller StaticPages home help
+  $ rails destroy  controller StaticPages home help
+  Undoes the controller
+
+  $ rails generate model User name:string email:string
+  $ rails destroy model User
+  Undoes the model
+
+  $ rails db:migrate
+  Changes the state of the database to support the current models & etc.
+  $ rails db:rollback
+  Undoes one db:migrate
+  $ rails db:migrate VERSION=0
+  Undoes all db:migrate (s), or reverts to the db VERSION # for #>0
+=end
 end
