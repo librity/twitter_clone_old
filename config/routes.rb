16563fea2cecf84da9e55c0befe0b932adaf6e9f
Librity <luis.geniole@gmail.com>
2019-08-25 17:02:43 -0300

    Finish Chapter 3; add contact, emojis, & dynamic titles && universal styling by passing instance variables to application.html.erb; also provide & yield

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 
-  
+
 
   #GET, POST, PATCH, DELETE: Basic HTTP operations between Client & Server, & fundamental of REST API architectures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#test0'
-  root 'static_pages#home'
+  
 end
